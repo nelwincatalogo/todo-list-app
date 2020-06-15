d5a5b5e75eb7e4184af9b627d7e3600b1ef91397
Nelwin <nelwin.catalogo@my.jru.edu>
2020-06-15 10:52:29 +0800

    added validation on add component

--- frontend/src/components/add.component.jsx
+++ frontend/src/components/add.component.jsx
@@ -3,12 +3,33 @@ import React, { Component } from "react";
 class Add extends Component {
   state = {
     description: "",
+    isError: false,
   };
 
   render() {
     return (
       <div className="row justify-content-center">
         <div className="col-8">
+          {this.state.isError ? (
+            <div
+              className="alert alert-danger alert-dismissible fade show"
+              role="alert"
+            >
+              Please enter some text on the new task.
+              <button
+                className="close"
+                type="button"
+                data-dismiss="alert"
+                aria-label="Close"
+                onClick={() => this.setState({ isError: false })}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+
           <div className="input-group">
             <input
               type="text"
@@ -21,6 +42,11 @@ class Add extends Component {
             <div className="input-group-append">
               <button
                 onClick={() => {
+                  if (this.state.description.length === 0) {
+                    this.setState({ isError: true });
+                    return;
+                  }
+
                   this.setState({ description: "" });
                   return this.props.onAdd(this.state.description);
                 }}
